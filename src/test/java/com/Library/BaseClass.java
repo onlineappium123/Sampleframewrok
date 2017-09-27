674c9212a2e97f363784d2005874465560eac8bd
onlineappium123 <onlineappium123@gmail.com>
2017-09-27 09:34:17 +0530

    first commit

---
+++ src/test/java/com/Library/BaseClass.java
@@ -0,0 +1,52 @@
+package com.Library;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
+import com.kirwa.nxgreport.NXGReports;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	
+	@BeforeMethod
+	public void setup(){
+		
+		if(GenericClass.getproperties("BROWSERNAME").equalsIgnoreCase("Chrome")){
+			
+			System.setProperty("webdriver.chrome.driver", "D:\\SeleniumFramework\\Framework\\BrowserDriver\\chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.get(GenericClass.getproperties("URL"));
+			driver.manage().window().maximize();
+		
+		}
+		else if(GenericClass.getproperties("BROWSERNAME").equals("firefox")){
+			
+			System.setProperty("webdriver.gecko.driver", "./BrowserDriver/geckodriver.exe");
+			driver=new FirefoxDriver();
+			driver.get(GenericClass.getproperties("URL"));
+			driver.manage().window().maximize();
+			
+		}
+		NXGReports.setWebDriver(driver);
+		
+	}
+	
+	@AfterMethod
+	public void teardown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+
+}
